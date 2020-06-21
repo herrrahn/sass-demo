191497625b7825680e1935ff5f15aa453dba299a
Rafael Rahn <rafael.rahn@live.com>
2020-06-21 15:17:28 +0200

    .

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {Page1Component} from './page1/page1.component';
+
+
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'alert'},
+  {path: 'page1', component: Page1Component},
+  {path: '**', component: Page1Component},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
